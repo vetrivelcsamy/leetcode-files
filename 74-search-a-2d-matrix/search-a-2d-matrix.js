ebbd62a19820058ab204d421beccced83ad2c1c1
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2024-01-22 10:00:04 +0530

    Time: 53 ms (58.51%) | Memory: 49.1 MB (5.17%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    for (let i = 0; i < matrix.length; i++) {
+        let left = 0;
+        let right = matrix[i].length - 1;
+
+        while (left <= right) {
+            const mid = Math.floor(left + (right - left) / 2);
+
+            if (matrix[i][mid] === target) {
+                return true;
+            }
+
+            if (matrix[i][mid] > target) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+    }
+    return false;
+};
