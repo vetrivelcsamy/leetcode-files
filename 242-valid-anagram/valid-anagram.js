b2df12967be22d26f3413c08c9d99c8f36c588cf
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2024-01-23 10:23:09 +0530

    Time: 59 ms (91.35%) | Memory: 50.8 MB (5.06%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if(s.length !== t.length){
+    return false;  
+  }
+  
+  let hashmapS = {};
+  let hashmapT = {};
+
+
+  for(let i = 0; i < s.length; i++){
+    hashmapS[s[i]] = (hashmapS[s[i]] || 0) + 1;  
+     hashmapT[t[i]] = (hashmapT[t[i]] || 0) + 1;  
+  }  
+
+  for(let index in hashmapS){
+   if(hashmapS[index] !== hashmapT[index]){
+     return false;  
+   } 
+  }
+
+  return true;
+};
