228619b5ae38ec1d269a436954cf0aa0bad52d04
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2024-01-24 11:01:38 +0530

    Time: 78 ms (20.26%) | Memory: 55.7 MB (5.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,27 +3,27 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  let left = 0;
+  let left =  0;
   let right = s.length - 1;
 
   while(left < right){
-    while(left < right && !isVaildString(s[left])){
-      left++
+    while(left < right && !isValidChar(s[left])){
+      left++  
     }
 
-    while(left < right && !isVaildString(s[right])){
-      right--
+    while(left < right && !isValidChar(s[right])){
+      right--  
     }
 
-    if(s[left].toLowerCase() !== s[right].toLowerCase()){
-     return false;
+    while(s[left].toLowerCase() !== s[right].toLowerCase()){
+      return false;  
     }
     left++;
-    right--;
-  } 
-  return true; 
+    right--;   
+  }  
+  return true
 };
 
-function isVaildString(char){
-  return /[a-zA-Z0-9]/.test(char)  
+function isValidChar(char){
+  return /[a-zA-Z0-9]/.test(char);
 }
