3a1ebbc5e81718c41546f051f1afb02f38a52ee5
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2024-01-24 10:41:26 +0530

    Time: 50 ms (70.95%) | Memory: 48.9 MB (5.05%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -3,22 +3,21 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
-   let left = 0;
-   let right = nums.length - 1;
+var searchInsert = function (nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
 
-   while(left <= right){
-     const mid = Math.floor(left + (right - left) / 2);
+    while (low <= high) {
+        const mid = Math.floor(low + (high - low) / 2);
 
-     if(nums[mid] === target){
-       return mid;  
-     }   
-
-     if(nums[mid] > target){
-       right = mid - 1  
-     } else{
-       left = mid + 1;   
-     }
-   } 
-   return left;
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low;
 };
