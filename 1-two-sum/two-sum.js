94a09c73b67002a2dc0f6e74cae846b8d7a10a39
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2024-01-25 11:51:31 +0530

    Time: 49 ms (95.07%) | Memory: 49.3 MB (7.23%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,14 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  let hashmap = {};
+var twoSum = function (nums, target) {
+    let hashmap = {};
 
-  for(let i = 0; i < nums.length; i++){
-    const sum = hashmap[target - nums[i]];
-    if(sum !== undefined){
-      return [sum, i];  
+    for (let i = 0; i < nums.length; i++) {
+        const sum = hashmap[target - nums[i]];
+
+        if (sum !== undefined) {
+            return [sum, i];
+        }
+
+        hashmap[nums[i]] = i;
     }
-    hashmap[nums[i]] = i   
-  } 
 };
